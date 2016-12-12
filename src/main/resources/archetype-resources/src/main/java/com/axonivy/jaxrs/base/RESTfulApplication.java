2424bd73ee430cab2f41ef3ba02609af3d790e01
genzer <genzers@gmail.com>
2016-12-12 20:27:07 +0700

    Initial project. A simple maven archetype to creat a basic JAX-RS application possibly running startup on Wildfly and run immediately using maven-wildfly-plugin.

---
+++ src/main/resources/archetype-resources/src/main/java/com/axonivy/jaxrs/base/RESTfulApplication.java
@@ -0,0 +1,9 @@
+package com.axonivy.jaxrs.base;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RESTfulApplication extends Application {
+
+}
